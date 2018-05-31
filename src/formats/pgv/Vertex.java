9eb8acde234e89d6beb019877695dd611f2759a4
João Cardoso <jaliborc@arenajunkies.com>
2018-05-31 23:46:23 +0100

    Moved java model analyser to independent repo

---
+++ src/formats/pgv/Vertex.java
@@ -0,0 +1,16 @@
+package formats.pgv;
+import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector2f;
+
+import math.Vector;
+
+public class Vertex {
+	public Vertex(int index) {
+		this.index = index;
+	}
+	
+	public Matrix4f visibility = new Matrix4f();
+	public Vector position = new Vector();
+	public Vector normal = new Vector();
+	public int index;
+}
